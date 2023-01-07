918534822e20df42efe14bcbd723c1a1b6f51ec5
Jasper Zimmer <jasper.z@posteo.de>
2023-01-07 13:55:11 +0100

    BOOTSTRAP FINISHED

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,4 +1,7 @@
 terraform {
-  backend "local" {
+  backend "s3" {
+    bucket = "codepipeline-operation-artifacts-20230107125411634900000002"
+    key    = "operation/terraform.tfstate"
+    region = "eu-central-1"
   }
 }
--- terraform/local_file_backend.tf
+++
@@ -1,9 +0,0 @@
-resource "local_file" "foo" {
-  content = templatefile(
-    "${path.module}/templates/backend.tf.tftpl",
-    {
-      bucket = aws_s3_bucket.codepipeline_operation_artifacts.id
-    }
-  )
-  filename = "${path.module}/../generated/backend.tf"
-}
